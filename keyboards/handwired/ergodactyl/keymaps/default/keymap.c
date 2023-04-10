71d8292fb88aed6df4dd460b81fac0a39b37c9d1
Purwa Astawa <purwa@crazyslug.net>
2023-04-10 23:10:34 +0800

    update keymaps

--- keyboards/handwired/ergodactyl/keymaps/default/keymap.c
+++ keyboards/handwired/ergodactyl/keymaps/default/keymap.c
@@ -2,10 +2,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC   , KC_1   , KC_2   , KC_3   , KC_4  , KC_5  , KC_F12  ,                   KC_PSCR  , KC_6     , KC_7     , KC_8    , KC_9           , KC_0            , KC_EQL  ,
+        KC_ESC  , KC_1   , KC_2   , KC_3   , KC_4  , KC_5  , KC_F12  ,                   KC_PSCR  , KC_6     , KC_7     , KC_8    , KC_9           , KC_0            , KC_EQL  ,
         KC_TAB   , KC_Q   , KC_W   , KC_E   , KC_R  , KC_T  , KC_LBRC ,                   KC_RBRC  , KC_Y     , KC_U     , KC_I    , KC_O           , KC_P            , KC_MINUS,
         KC_LALT  , KC_A   , KC_S   , KC_D   , KC_F  , KC_G  , KC_PIPE ,                   KC_BSLS  , KC_H     , KC_J     , KC_K    , KC_L           , KC_SCLN         , KC_QUOTE,
-        KC_LCTL  , KC_Z   , KC_X   , KC_C   , KC_V  , KC_B  ,                                        KC_N     , KC_M     , KC_COMM , KC_DOT         , KC_SLSH         , KC_LCTL ,
+        KC_LCTL  , KC_Z   , KC_X   , KC_C   , KC_V  , KC_B  ,                                        KC_N     , KC_M     , KC_COMM , KC_DOT         , KC_SLSH         , KC_BSLS ,
         MO(1)    , KC_HOME, KC_WH_L, KC_WH_D,                                                                              KC_WH_U , KC_WH_R        , KC_END          , KC_DEL ,
         LSFT_T(KC_DEL),ALT_T(KC_DOWN),KC_LSFT, KC_SPC ,  CTL_T(KC_LEFT), KC_LGUI,                       KC_GRV   , CTL_T(KC_RIGHT)  ,  KC_ENT ,  KC_ESC, ALT_T(KC_UP) ,KC_BSPC
     ),
