a585d733ce26087667aba76e35d2e005f02766a9
Purwa Astawa <purwa@crazyslug.net>
2025-10-06 08:33:11 +0800

    tightyl

--- keyboards/handwired/tightyl/keymaps/default/keymap.c
+++ keyboards/handwired/tightyl/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 #define XXX KC_NO
 
 // Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
+#define HOME_A LCTL_T(KC_A)
 #define HOME_S LALT_T(KC_S)
 #define HOME_D LCTL_T(KC_D)
 #define HOME_F LSFT_T(KC_F)
@@ -26,7 +26,7 @@
 #define HOME_J RSFT_T(KC_J)
 #define HOME_K RCTL_T(KC_K)
 #define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RGUI_T(KC_SCLN)
+#define HOME_SCLN LCTL_T(KC_SCLN)
 
 
 // Left-hand home row mods
@@ -47,28 +47,28 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_ESC   , KC_Q   , KC_W   , KC_E   , KC_R        , KC_T  ,                     KC_Y     , KC_U        , KC_I    , KC_O           , KC_P            , KC_MINUS,
-        KC_LALT  , KC_A , HOME_S , HOME_D , HOME_F      , KC_G  ,                       KC_H     , HOME_J      , HOME_K  , HOME_L         , KC_SCLN       , KC_QUOTE,
+        KC_LALT  , HOME_A , HOME_S , HOME_D , HOME_F      , KC_G  ,                       KC_H     , HOME_J      , HOME_K  , HOME_L         , HOME_SCLN       , KC_QUOTE,
         KC_LSFT  , KC_Z   , KC_X   , KC_C   , KC_V        , KC_B  ,                     KC_N     , KC_M        , KC_COMM , KC_DOT         , KC_SLSH         , KC_BSLS ,
                                       LT(_CONFIG, KC_DEL),  KC_LGUI,                    KC_LCBR  , KC_RCBR,
                   LT(_NAV, KC_GRV),    KC_SPC,  LT(_FUN, KC_TAB),                       LT(_SYM, KC_ENT), KC_LSFT, LT(_NUM, KC_BSPC)
     ),
     [_NAV] = LAYOUT(
         KC_TAB   ,     XXX,     XXX,     XXX,          XXX,     XXX,                    KC_HOME  , KC_PGUP     , KC_PGDN , KC_END         , XXX             , KC_EQL,
-        XXX      , KC_LGUI, KC_LALT, KC_LCTL,       KC_LSFT,    XXX,                    KC_LEFT  , KC_DOWN     , KC_UP   , KC_RIGHT       , XXX             , KC_UNDS,
+        XXX      , KC_LCTL, KC_LALT, KC_LCTL,       KC_LSFT,    XXX,                    KC_LEFT  , KC_DOWN     , KC_UP   , KC_RIGHT       , XXX             , KC_UNDS,
         XXX      ,     XXX,     XXX,     XXX,           XXX,    XXX,                    XXX      , XXX         , XXX     , XXX            , XXX             , KC_PIPE,
                                                   KC_TRNS,  KC_TRNS,                    KC_BSPC  , KC_DEL,
                                       KC_TRNS,    KC_TRNS,  KC_TRNS,                    KC_TRNS  , KC_TRNS, KC_TRNS
     ),
     [_FUN] = LAYOUT(
         KC_TAB   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS     , KC_TRNS,                    KC_F1    , KC_F2       , KC_F3   , KC_F4          , KC_F5           , KC_F6,
-        KC_TRNS  , KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT     , KC_TRNS,                    KC_F7    , KC_F8       , KC_F9   , KC_F10          , KC_F11          , KC_F12,
+        KC_TRNS  , KC_LCTL, KC_LALT, KC_LCTL, KC_LSFT     , KC_TRNS,                    KC_F7    , KC_F8       , KC_F9   , KC_F10          , KC_F11          , KC_F12,
         KC_TRNS  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS     , KC_PSCR,                    KC_PSCR  , KC_TRNS     , KC_TRNS , KC_TRNS        , KC_TRNS         , KC_TRNS,
                                                   KC_TRNS,  KC_TRNS,                    KC_TRNS  , KC_TRNS,
                                       KC_TRNS,    KC_TRNS,  KC_TRNS,                    KC_TRNS  , KC_TRNS, KC_TRNS
     ),
     [_NUM] = LAYOUT(
         KC_PLUS  , KC_MINUS, KC_7   , KC_8   , KC_9        , KC_0   ,                   KC_LEFT  , KC_DOWN     , KC_UP   , KC_RIGHT       , XXX             ,  KC_EQL,
-        KC_ASTR  , KC_SLSH , KC_4   , KC_5   , KC_6        , KC_LBRC,                   XXX      , KC_LSFT     , KC_LCTL , KC_LALT        , KC_LGUI         ,  XXX,
+        KC_ASTR  , KC_SLSH , KC_4   , KC_5   , KC_6        , KC_LBRC,                   XXX      , KC_LSFT     , KC_LCTL , KC_LALT        , KC_LCTL         ,  XXX,
         KC_EQL   , KC_PERC , KC_1   , KC_2   , KC_3        , KC_RBRC,                   XXX      , XXX         , XXX     , XXX            , XXX             ,  XXX,
                                                   KC_COMM  ,  KC_DOT,                   KC_TRNS  , KC_TRNS,
                                       KC_PIPE,    KC_TRNS,  KC_AMPR,                    KC_TRNS  , KC_TRNS, KC_TRNS
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYM] = LAYOUT(
         KC_GRV   , KC_EXLM , KC_QUES, KC_SLSH, KC_HASH      , KC_CIRC,                  KC_LEFT  , KC_DOWN     , KC_UP   , KC_RIGHT       , XXX             ,  KC_EQL,
-        KC_PERC  , KC_LPRN , KC_EQL , KC_GT  , KC_LCBR      , KC_LBRC,                    XXX      , KC_LSFT     , KC_LCTL , KC_LALT        , KC_LGUI         ,  XXX,
+        KC_PERC  , KC_LPRN , KC_EQL , KC_GT  , KC_LCBR      , KC_LBRC,                    XXX      , KC_LSFT     , KC_LCTL , KC_LALT        , KC_LCTL         ,  XXX,
         KC_DLR   , KC_RPRN , KC_MINUS, KC_LT, KC_RCBR       , KC_RBRC,                  XXX      , XXX         , XXX     , XXX            , XXX             ,  XXX,
                                                   KC_COMM   ,  KC_DOT,                  KC_TRNS , KC_TRNS,
                                       KC_PIPE,    KC_TRNS,  KC_AMPR,                    KC_TRNS  , KC_TRNS, KC_TRNS
